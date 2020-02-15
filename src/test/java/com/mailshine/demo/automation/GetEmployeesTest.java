08d6f41962e8f1839e906292ed4d70aa777d00c9
mailshine <mailshine@gmail.com>
2020-02-15 23:06:02 +0000

    Adding Cucumber implement steps to avoid cucumber implement steps error

---
+++ src/test/java/com/mailshine/demo/automation/GetEmployeesTest.java
@@ -0,0 +1,17 @@
+package com.mailshine.demo.automation;
+
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class GetEmployeesTest {
+
+    @When("^I perform Get Employees Rest API call against the demo endpoint$")
+    public void getEmployeeApiCall() {
+        // TODO with soapui junit
+    }
+
+    @Then("^I should get a list of employees successfully$")
+    public void getEmployeeApiAssertion()  {
+        // TODO with soapui junit
+    }
+}
